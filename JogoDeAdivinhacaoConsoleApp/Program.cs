b8420e96a19976dda3fe1553599e8eaf6f63229a
Deivissep <daavi2223@gmail.com>
2025-03-20 20:24:47 -0300

    Implementado a versão 01 - coleta de dados

--- JogoDeAdivinhacaoConsoleApp/Program.cs
+++ JogoDeAdivinhacaoConsoleApp/Program.cs
@@ -2,9 +2,26 @@
 {
     internal class Program
     {
+        //V2.0 - Gerar numero secreto aleatório
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("-------------------------------------");
+            Console.WriteLine("Jogo de Adivinhação");
+            Console.WriteLine("-------------------------------------");
+
+
+            //lógica do jogo
+            Random geradorDeNumeros = new Random();
+
+            int numeroSecreto = geradorDeNumeros.Next(1, 21);
+
+            Console.Write("Digite um numero (de 1 à 20) para chutar: ");
+            int numeroDigitado = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Você digitou o número: " + numeroDigitado);
+            Console.WriteLine("O numero secreto é: " + numeroSecreto);
+
+            Console.ReadLine();
         }
     }
 }
